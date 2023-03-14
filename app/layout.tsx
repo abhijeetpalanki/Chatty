97e0bcf5135f6bf6b7305cae2b8205f6c8909abc
abhijeetpalanki <ajpalanki@gmail.com>
2023-03-13 22:58:45 -0500

    implemted message func with toast notification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { getServerSession } from "next-auth";
+import ClientProvider from "../components/ClientProvider";
 import Login from "../components/Login";
 import { SessionProvider } from "../components/SessionProvider";
 import Sidebar from "../components/Sidebar";
@@ -24,6 +25,8 @@ export default async function RootLayout({
               <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
                 <Sidebar />
               </div>
+
+              <ClientProvider />
 
               <div className="bg-[#343541] flex-1">{children}</div>
             </div>
